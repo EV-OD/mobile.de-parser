f8d2bd3588c6153972fcb177d5031d2ced56d8d7
EV-OD <evod599@gmail.com>
2025-01-14 00:10:04 +0545

    feat: implement data saving functionality

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const UserAgent = require('user-agents');
 const { navigateToSearchInfoFromHome, navigateToEachSearchFilter } = require('./pages/navigate');
 const { getAllSearchespage, pageInfo } = require('./parsers/searchPagesInfo');
 const { isUserDataPresent } = require('./userData/utils');
+const { saveJson } = require('./utils/helpers');
 const ua = new UserAgent({ deviceCategory: "desktop" });
 const newUserAgent = ua.userAgent;
 
@@ -26,14 +27,17 @@ async function main() {
     // }else{
         page.goto('https://suchen.mobile.de/fahrzeuge/mymobile/searchOverview.html')
     // }
+
     let seachLinks = await getAllSearchespage(page, 3);
+    let datas = []
     for (const link of seachLinks) {
         console.log('Navigating to:', link);
         await navigateToEachSearchFilter(page, link);
         console.log('Navigated to:', link);
         let data = await pageInfo(page);
-        console.log(data);
+        datas.push(data);
     }
+    saveJson(datas, 'data.json');
 
     await browser.close();
 }
--- utils/helpers.js
+++ utils/helpers.js
@@ -1,5 +1,13 @@
+const fs = require('fs');
+
+
 const randomRange = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
+const saveJson = (data, fileName) => {
+    fs.writeFileSync(`./parseData/${fileName}`, JSON.stringify(data, null, 2));
+}
+
 module.exports = {
-    randomRange
+    randomRange,
+    saveJson
 }
